bf60bfc7cc06dba3c982ef8e475468b971f483e0
lina-22 <haquefahinur@gmail.com>
2022-10-30 19:03:23 +0100

    exercise practicing for the multiplication

---
+++ chapitre1/src/examChap1/Exercices11/Exo6.java
@@ -0,0 +1,17 @@
+package examChap1.Exercices11;
+
+import java.util.Scanner;
+
+public class Exo6 {
+    public static void main(String[] args) {
+        System.out.println("plz enter a number:");
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+            System.out.println("Le tableau de:" + nb +"est" );
+        for (int i = 1; i <=10 ; i++) {
+            int nb2 = i;
+            System.out.println(nb +"x" +i +"=" +nb*i);
+
+        }
+    }
+}
